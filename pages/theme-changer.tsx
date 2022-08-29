6dc114a96a5f63bba844d38a9fa4484abac1f86f
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-29 17:07:54 -0300

    estructura del ThemeChagerPage

--- pages/theme-changer.tsx
+++ pages/theme-changer.tsx
@@ -1,12 +1,44 @@
+import {
+   Card,
+   CardContent,
+   FormControl,
+   FormControlLabel,
+   FormLabel,
+   Radio,
+   RadioGroup,
+} from "@mui/material";
 import React from "react";
 import { Layout } from "../components/layouts";
 
 const ThemeChagerPage = () => {
    return (
-    <Layout>
-        <h1>ThemeChagerPage</h1>
-    </Layout>
-   )
+      <Layout>
+         <Card>
+            <CardContent>
+               <FormControl>
+                  <FormLabel>Tema</FormLabel>
+                  <RadioGroup>
+                     <FormControlLabel
+                        value="light"
+                        control={<Radio />}
+                        label="light"
+                     />
+                     <FormControlLabel
+                        value="dark"
+                        control={<Radio />}
+                        label="Dark"
+                     />
+                     <FormControlLabel
+                        value="custom"
+                        control={<Radio />}
+                        label="Custom"
+                     />
+                  </RadioGroup>
+               </FormControl>
+            </CardContent>
+         </Card>
+      </Layout>
+   );
 };
 
 export default ThemeChagerPage;
