ded0988e4095e2aecc9de456aee902178f9024b4
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-29 17:03:27 -0300

    about - contact - themeChanger

---
+++ pages/theme-changer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Layout } from "../components/layouts";
+
+const ThemeChagerPage = () => {
+   return (
+    <Layout>
+        <h1>ThemeChagerPage</h1>
+    </Layout>
+   )
+};
+
+export default ThemeChagerPage;
