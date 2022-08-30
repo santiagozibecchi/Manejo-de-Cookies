780d8ae673634e0e316e0fe654e6548aa95db5cd
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-08-29 22:24:12 -0300

    Establecer el theme usando la cookie - Frontend

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,39 @@
-import "../styles/globals.css";
+import { useState, useEffect } from "react";
 import type { AppContext, AppProps } from "next/app";
+import "../styles/globals.css";
+
 import { CssBaseline, Theme, ThemeProvider } from "@mui/material";
 import { darkTheme, lightTheme, customTheme } from "../themes";
+import Cookies from "js-cookie";
 
 interface Props extends AppProps {
    theme: string;
 }
 
-function MyApp({ Component, pageProps, theme }: Props) {
+function MyApp({ Component, pageProps, theme = "dark" }: Props) {
    // console.log({ theme });
+   // Inicialmente el theme por defecto cuando se renderice por primera
+   //  vez por el lado del client y servidor sera lightTheme
+   const [currentTheme, setCurrentTheme] = useState(lightTheme);
 
-   const currentTheme: Theme =
-      theme === "light"
-         ? lightTheme
-         : theme === "dark"
-         ? darkTheme
-         : customTheme;
+   // pero tan pronto como se dispare el efecto
+   useEffect(() => {
+      // Este efecto va a revisar la cookie
+      const cookieTheme = Cookies.get("theme") || "light";
+
+      const selectedTheme: Theme =
+         cookieTheme === "light"
+            ? lightTheme
+            : cookieTheme === "dark"
+            ? darkTheme
+            : customTheme;
+
+      setCurrentTheme(selectedTheme);
+   }, []);
+
+   // * Hay que asegurarse que el contenido que se genera en el lado del cliente
+   // * sea el mismo que se genera en el lado del servidor, caso contrario da un error
+   //  * de tipo did not match: Server
 
    return (
       <ThemeProvider theme={currentTheme}>
@@ -26,19 +44,20 @@ function MyApp({ Component, pageProps, theme }: Props) {
    );
 }
 
-// ! No es muy recomendable utilizar getInitialProps => Perdemos instantaneamente 
+// ! No es muy recomendable utilizar getInitialProps => Perdemos instantaneamente
 // ! el poder utilizar el staticGeneration
-MyApp.getInitialProps = async (appContext: AppContext) => {
-   const { theme } = appContext.ctx.req
-      ? (appContext.ctx.req as any).cookies
-      : { theme: "light" };
+// ! Por lo que, cada peticion se dirige al servidor y generara una respuesta
+// MyApp.getInitialProps = async (appContext: AppContext) => {
+//    const { theme } = appContext.ctx.req
+//       ? (appContext.ctx.req as any).cookies
+//       : { theme: "light" };
 
-   // console.log("getInitialProps", cookies);
-   const validThemes = ["light", "dark", "custom"];
+//    // console.log("getInitialProps", cookies);
+//    const validThemes = ["light", "dark", "custom"];
 
-   return {
-      theme: validThemes.includes(theme) ? theme : "dark",
-   };
-};
+//    return {
+//       theme: validThemes.includes(theme) ? theme : "dark",
+//    };
+// };
 
 export default MyApp;
